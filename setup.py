f4c6e6f7182d86db077d7ad6f4af7eb00a18fd69
salimm <salimm@cs.pitt.edu>
2018-01-22 05:01:25 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
-  url = 'https://github.com/LessRPC/lessrpc_stub_py2x/', # use the URL to the github repo
-  download_url = 'https://github.com/LessRPC/lessrpc_stub_py2x/archive/1.0.0.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/LessRPC/less_msgpack/', # use the URL to the github repo
+  download_url = 'https://github.com/LessRPC/less_msgpack/archive/1.0.0.tar.gz', # I'll explain this in a second
   keywords = ['python','serialization','deserialization','rpc','rmi','less rpc', 'client', 'server'], # arbitrary keywords
   classifiers = ['Programming Language :: Python '],
   install_requires=['pylods','pylods_msgpack'],
