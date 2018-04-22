5f01eb7cc2b9ca42ea4dd3c4fa20b14d1ab832bd
salimm <salimm@cs.pitt.edu>
2018-04-22 15:43:42 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name = 'lessrpc_msgpack',
   packages = ['lessrpc_msgpack'], # this must be the same as the name above
-  version = '1.0.3',
+  version = '1.0.4',
   description = 'Less RPC MsgPack extension',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/LessRPC/less_msgpack/', # use the URL to the github repo
-  download_url = 'https://github.com/LessRPC/less_msgpack/archive/1.0.3.tar.gz', # I'll explain this in a second
+  download_url = 'https://github.com/LessRPC/less_msgpack/archive/1.0.4.tar.gz', # I'll explain this in a second
   keywords = ['python','serialization','deserialization','rpc','rmi','less rpc', 'client', 'server'], # arbitrary keywords
   classifiers = ['Programming Language :: Python'],
   install_requires=['pylods','pylods_msgpack', 'lessrpc-common'],
